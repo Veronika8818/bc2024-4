0f748dbc0457f0bce0ac1f2d4b25dac8e4bc1d8f
Вероніка bc2024-4 <veronika.korol2020@gmail.com>
2024-10-21 10:45:30 +0300

    Додано зміни

--- index.js
+++ index.js
@@ -1,27 +1,62 @@
-const http = require('http'); 
+
+const http = require('http');
 const { program } = require('commander');
- const fs = require('fs');
- const path = require('path');
+const fs = require('fs').promises;
+const path = require('path');
+
 
 program
- .requiredOption('-h, --host <host>', 'Server host address') 
-.requiredOption('-p, --port <port>', 'Server port number')
-.requiredOption('-c, --cache <cacheDir>', 'Cache directory path') 
-.parse(process.argv);
+  .requiredOption('-h, --host <host>', 'Server host address')
+  .requiredOption('-p, --port <port>', 'Server port number')
+  .requiredOption('-c, --cache <cacheDir>', 'Cache directory path')
+  .parse(process.argv);
 
 const options = program.opts();
-
 const cacheDir = path.resolve(options.cache);
- if (!fs.existsSync(cacheDir)) { 
-console.error(`Error: Cache directory "${cacheDir}" does not exist.`);
- process.exit(1); 
-}
 
-const server = http.createServer((req, res) => { 
- res.writeHead(200); 
-res.end('Hello!\n');
- });
 
-server.listen(options.port, options.host, () => { 
-console.log(`Server running at http://${options.host}:${options.port}/`);
-});
+(async () => {
+  try {
+    await fs.access(cacheDir);
+  } catch (err) {
+    console.error(`Error: Cache directory "${cacheDir}" does not exist.`);
+    process.exit(1);
+  }
+
+  
+  const server = http.createServer(async (req, res) => {
+    const method = req.method;
+    const url = req.url;
+    const httpCode = url.slice(1); 
+    const filePath = path.join(cacheDir, `${httpCode}.jpg`);
+
+
+    if (method === 'GET') {
+      try {
+        const data = await fs.readFile(filePath);
+        res.writeHead(200, { 'Content-Type': 'image/jpeg' });
+        res.end(data);
+      } catch (err) {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('File not found\n');
+      }
+    } else {
+      res.writeHead(405, { 'Content-Type': 'text/plain' });
+      res.end('Method not allowed\n');
+    }
+  });
+
+  
+  server.listen(options.port, options.host, () => {
+    console.log(`Server running at http://${options.host}:${options.port}/`);
+  });
+})();
+
+
+
+
+
+
+
+
+
